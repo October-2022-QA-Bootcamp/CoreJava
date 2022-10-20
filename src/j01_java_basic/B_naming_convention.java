2fe8e7a9692e310f555cc8e1742deeeaa9df5c22
TofaelKabir <tofaelkabir@gmail.com>
2022-10-20 19:27:47 -0400

    updated code

--- src/j01_java_basic/B_naming_convention.java
+++ src/j01_java_basic/B_naming_convention.java
@@ -1,23 +1,20 @@
 package j01_java_basic;
 
-// Projects names generally starts with UpperCase -- October2022CoreJava
+// Projects names generally starts with UpperCase --> October2022CoreJava
+// Package names are generally starts with lowerCase --> j01JavaBasic
+// Class names starts with UpperCase. Class names are usually nouns or noun phrases --> NamingConvention
+
+// Project, package, class follow Camel case or Snake case pattern.
+// Camel case feature considered from second word, not from first word
 // Camel case example for a project --> October2022CoreJava
-// Camel case feature considered from second word, not from first word
-// Alternative example of Camel case (Called snake_case), for a project --> October_2022_core_java
-// Snake case feature considered from second word, not from first word
-
-// Package names are generally starts with lowerCase
 // Camel case example for a package --> jv01JavaBasic
-// Camel case feature considered from second word, not from first word
-// Alternative example of Camel case (Called snake_case), for a package --> jv01_java_basic
-// Snake case feature considered from second word, not from first word
-
-// Class names starts with UpperCase 
-// Class names are usually nouns or noun phrases.
-// Camel case feature considered from second word, not from first word
 // Camel case example for a class --> NamingConvention
-// Alternative example of Camel case (Called snake_case), for a class --> Naming_convention
-// Snake case feature considered from second word, not from first word
+
+// Alternative example of Camel case (Called snake_case).
+// Snake case feature considered from second word, not from first word like Came case feature
+// Snake case example for a project --> October_2022_Core_Java
+// Snake case example for a package --> jv_01_Java_Basic
+// Snake case example for a class --> Naming_Convention
 
 public class B_naming_convention {
 
